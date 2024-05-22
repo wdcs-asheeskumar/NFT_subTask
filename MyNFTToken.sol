863f90d1f63eaadb5364a0a73840c7c47a8d5ef6
wdcs-asheeskumar <154244737+wdcs-asheeskumar@users.noreply.github.com>
2024-05-22 12:41:31 +0530

    Update MyNFTToken.sol

--- MyNFTToken.sol
+++ MyNFTToken.sol
@@ -16,6 +16,8 @@ contract MyNFTToken is ERC1155, Ownable, ERC1155Burnable, ERC20 {
         bool goldNFTT1;
         bool blackNftT2;
         bool goldNFTT2;
+        uint256 blackNftTCount;
+        uint256 goldNFTTCount;
     }
     /// @dev  Struct to store NFT's data of the users who are included whitelisted in tier 1
     struct whiteListingTier1 {
@@ -37,34 +39,23 @@ contract MyNFTToken is ERC1155, Ownable, ERC1155Burnable, ERC20 {
     mapping(address => whiteListingTier1) public whiteListTier1Record;
     /// @dev mapping the NFT's data of users who are included in tier 2
     mapping(address => whiteListingTier2) public whiteListTier2Record;
+    /// @dev mapping for the time at which the user is getting whitelisted
+    mapping(address => uint256) public whiteListedUsersTimes;
 
     address internal initialOwner;
-    address payable addr = payable(address(this));
+    address payable addr = payable(msg.sender);
     /// @dev declaring the variables for the different time frames
-    uint256 public timeFrame1;
-    uint256 public timeFrame2;
-    uint256 public timeFrame3;
-    uint256 public timeFrame4;
-    uint256 public timeFrame5;
-    uint256 public timeFrame6;
-    uint256 public num;
-
-    event Timeframes(
-        uint256 timeFrame1,
-        uint256 timeFrame2,
-        uint256 timeFrame3,
-        uint256 timeFrame4,
-        uint256 timeFrame5,
-        uint256 timeFrame6
-    );
+    uint256 private timeFrame1 = 0;
+    uint256 private timeFrame2 = 30 minutes;
+    uint256 private timeFrame3 = 60 minutes;
+    uint256 private timeFrame4 = 90 minutes;
+    uint256 private timeFrame5 = 120 minutes;
+    uint256 private timeFrame6 = 150 minutes;
 
     /// @dev constructor to initialize the oqner, uri, token's name and setting up the timeframes
-    constructor()
-        ERC1155("ipfs://QmZQmq7m2E9AeAh2sPLJmt8jL8MRrTMLqdTsX59V6wj1Wu/")
-        ERC20("MyToken", "MTK")
-        Ownable(msg.sender)
-    {
+    constructor() ERC1155("") ERC20("MyNFTToken", "MNTK") Ownable(msg.sender) {
         initialOwner = msg.sender;
+        _mint(address(this), (10 ** decimals()));
     }
 
     /// @dev function for updating the uri
@@ -72,60 +63,9 @@ contract MyNFTToken is ERC1155, Ownable, ERC1155Burnable, ERC20 {
         _setURI(newuri);
     }
 
-    /// @dev function for minting NFTs and it's only accessible to the owner
-    function mintNFT(
-        address account,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) public {
-        _mint(account, id, amount, data);
-    }
-
-    /// @dev function to mint a batch of NFTS. It is only accessible to the owner.
-    function mintBatch(
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public onlyOwner {
-        _mintBatch(to, ids, amounts, data);
-    }
-
-    /// @dev Function to mint the ERC 20 tokens
-    function mintToken(uint256 _amount) public {
-        _mint(address(this), _amount * (10 ** decimals()));
-    }
-
     /// @dev Function to check total token balance
     function totalTokens() public view returns (uint256) {
         return balanceOf(address(this));
-    }
-
-    /// @dev Function to set timeframes
-    function setTimeFrames(
-        uint256 _timeFrame1,
-        uint256 _timeFrame2,
-        uint256 _timeFrame3,
-        uint256 _timeFrame4,
-        uint256 _timeFrame5,
-        uint256 _timeFrame6
-    ) public {
-        timeFrame1 = _timeFrame1;
-        timeFrame2 = _timeFrame2;
-        timeFrame3 = _timeFrame3;
-        timeFrame4 = _timeFrame4;
-        timeFrame5 = _timeFrame5;
-        timeFrame6 = _timeFrame6;
-
-        emit Timeframes(
-            _timeFrame1,
-            _timeFrame2,
-            _timeFrame3,
-            _timeFrame4,
-            _timeFrame5,
-            _timeFrame6
-        );
     }
 
     /// @dev Funtion to register user in the pool. It is accessible to the users.
@@ -133,8 +73,10 @@ contract MyNFTToken is ERC1155, Ownable, ERC1155Burnable, ERC20 {
         address _accountAddress,
         bool _isPartOfTier1,
         bool _isPartOfTier2,
-        bool _isBlackNft,
-        bool _isGoldNft
+        bool _isBlackNft1,
+        bool _isGoldNft1,
+        bool _isBlackNft2,
+        bool _isGoldNft2
     ) public {
         require(_accountAddress != address(0), "Invalid address");
         require(
@@ -144,29 +86,22 @@ contract MyNFTToken is ERC1155, Ownable, ERC1155Burnable, ERC20 {
         userList[_accountAddress] = true;
         if (_isPartOfTier1 == true) {
             userData[_accountAddress].partOfTier1 = true;
-            if (_isBlackNft == true) {
+            if (_isBlackNft1 == true) {
                 userData[_accountAddress].blackNftT1 = true;
             }
-            if (_isGoldNft == true) {
+            if (_isGoldNft1 == true) {
                 userData[_accountAddress].goldNFTT1 = true;
             }
         }
         if (_isPartOfTier2 == true) {
             userData[_accountAddress].partOfTier2 = true;
-            if (_isBlackNft == true) {
+            if (_isBlackNft2 == true) {
                 userData[_accountAddress].blackNftT2 = true;
             }
-            if (_isGoldNft == true) {
+            if (_isGoldNft2 == true) {
                 userData[_accountAddress].goldNFTT2 = true;
             }
         }
-    }
-
-    /// @dev Function to view the users added in the user list
-    function viewAddedUserInTheList(
-        address _accountAddress
-    ) public view returns (bool) {
-        return userList[_accountAddress];
     }
 
     /// @dev  Function to white list the users.
@@ -184,20 +119,122 @@ contract MyNFTToken is ERC1155, Ownable, ERC1155Burnable, ERC20 {
                 (userData[_accountAddress].partOfTier2 == true),
             "The user is not intrested in purchasing any NFT"
         );
-        if (userData[_accountAddress].blackNftT1 == true) {
-            whiteListTier1Record[_accountAddress].blackNftWT1 = true;
+        whiteListedUsersTimes[_accountAddress] = block.timestamp;
+        if (userData[_accountAddress].partOfTier1 == true) {
+            if (userData[_accountAddress].blackNftT1 == true) {
+                whiteListTier1Record[_accountAddress].blackNftWT1 = true;
+            }
+            if (userData[_accountAddress].goldNFTT1 == true) {
+                whiteListTier1Record[_accountAddress].goldNFTWT1 = true;
+            }
         }
-        if (userData[_accountAddress].goldNFTT1 == true) {
-            whiteListTier1Record[_accountAddress].goldNFTWT1 = true;
-        }
-
-        if (userData[_accountAddress].blackNftT2 == true) {
-            whiteListTier2Record[_accountAddress].blackNftWT2 = true;
-        }
-        if (userData[_accountAddress].goldNFTT2 == true) {
-            whiteListTier2Record[_accountAddress].goldNFTWT2 = true;
+        if (userData[_accountAddress].partOfTier2 == true) {
+            if (userData[_accountAddress].blackNftT2 == true) {
+                whiteListTier2Record[_accountAddress].blackNftWT2 = true;
+            }
+            if (userData[_accountAddress].goldNFTT2 == true) {
+                whiteListTier2Record[_accountAddress].goldNFTWT2 = true;
+            }
         }
         whiteListedUsers[_accountAddress] = true;
+    }
+
+    function buyAndRedeemTimeFrame(
+        address _accountAddress,
+        uint256 _valueOfBlackNft,
+        uint256 _valueOfGoldNft
+    ) external payable {
+        require(
+            whiteListedUsers[_accountAddress] == true,
+            "Only whitelisted users can access this functionality."
+        );
+
+        if (
+            block.timestamp - whiteListedUsersTimes[_accountAddress] >
+            timeFrame1 &&
+            block.timestamp - whiteListedUsersTimes[_accountAddress] <
+            timeFrame2
+        ) {
+            require(
+                _valueOfBlackNft + _valueOfGoldNft <= 5,
+                "You can't buy more than 5 NFTs"
+            );
+
+            _mint(_accountAddress, 0, _valueOfBlackNft, "0x0");
+            addr.transfer(_valueOfBlackNft * 10000000000000000 wei);
+
+            _mint(_accountAddress, 1, _valueOfGoldNft, "0x0");
+            addr.transfer(_valueOfGoldNft * 20000000000000000 wei);
+            userData[_accountAddress].blackNftTCount = _valueOfBlackNft;
+            userData[_accountAddress].goldNFTTCount = _valueOfGoldNft;
+        } else if (
+            block.timestamp - whiteListedUsersTimes[_accountAddress] >
+            timeFrame2 &&
+            block.timestamp - whiteListedUsersTimes[_accountAddress] <
+            timeFrame3
+        ) {
+            require(
+                _valueOfBlackNft + _valueOfGoldNft <= 3,
+                "You can't buy more than 3 NFTs"
+            );
+
+            _mint(_accountAddress, 0, _valueOfBlackNft, "0x0");
+            addr.transfer(_valueOfBlackNft * 10000000000000000 wei);
+
+            _mint(_accountAddress, 1, _valueOfGoldNft, "0x0");
+            addr.transfer(_valueOfGoldNft * 20000000000000000 wei);
+            userData[_accountAddress].blackNftTCount = _valueOfBlackNft;
+            userData[_accountAddress].goldNFTTCount = _valueOfGoldNft;
+        } else if (
+            block.timestamp - whiteListedUsersTimes[_accountAddress] >
+            timeFrame3 &&
+            block.timestamp - whiteListedUsersTimes[_accountAddress] <
+            timeFrame4
+        ) {
+            uint256 tokenForBlackandGoldNFT = (25 *
+                userData[_accountAddress].blackNftTCount) +
+                (75 * userData[_accountAddress].goldNFTTCount);
+            transferFrom(
+                address(this),
+                _accountAddress,
+                tokenForBlackandGoldNFT
+            );
+        } else if (
+            block.timestamp - whiteListedUsersTimes[_accountAddress] >
+            timeFrame4 &&
+            block.timestamp - whiteListedUsersTimes[_accountAddress] <
+            timeFrame5
+        ) {
+            uint256 tokenForBlackandGoldNFT = (30 *
+                userData[_accountAddress].blackNftTCount) +
+                (90 * userData[_accountAddress].goldNFTTCount);
+            transferFrom(
+                address(this),
+                _accountAddress,
+                tokenForBlackandGoldNFT
+            );
+        } else if (
+            block.timestamp - whiteListedUsersTimes[_accountAddress] >
+            timeFrame5 &&
+            block.timestamp - whiteListedUsersTimes[_accountAddress] <
+            timeFrame6
+        ) {
+            uint256 tokenForBlackandGoldNFT = (50 *
+                userData[_accountAddress].blackNftTCount) +
+                (150 * userData[_accountAddress].goldNFTTCount);
+            transferFrom(
+                address(this),
+                _accountAddress,
+                tokenForBlackandGoldNFT
+            );
+        }
+    }
+
+    /// @dev Function to view the users added in the user list
+    function viewAddedUserInTheList(
+        address _accountAddress
+    ) public view returns (bool) {
+        return userList[_accountAddress];
     }
 
     /// @dev Function to view the users addred in the whitelist
@@ -205,149 +242,5 @@ contract MyNFTToken is ERC1155, Ownable, ERC1155Burnable, ERC20 {
         address _accountAddress
     ) public view returns (bool) {
         return whiteListedUsers[_accountAddress];
-    }
-
-    /// @dev function for the whitelisted users to buy NFT's between time frame 1 and time frame 2.
-    function userGetsNFTTF1(
-        address _to,
-        uint256 _id,
-        uint256 _valueBlackNft,
-        uint256 _valueGoldNft
-    ) public payable returns (uint256) {
-        // require(
-        //     block.timestamp >= timeFrame1 && block.timestamp < timeFrame2,
-        //     "The time frame is over."
-        // );
-        require(
-            whiteListedUsers[_to] == true,
-            "Address is not whitlisted and hence it can't buy NFT"
-        );
-        require(
-            (_valueBlackNft + _valueGoldNft) <= 5,
-            "User can't buy more than 5 NFTs in this time frame."
-        );
-        if (_id == 1) {
-            safeTransferFrom(address(this), _to, _id, _valueBlackNft, "0x0");
-            addr.transfer(10000000000000000 wei);
-        }
-        if (_id == 2) {
-            safeTransferFrom(address(this), _to, _id, _valueGoldNft, "0x0");
-            addr.transfer(20000000000000000 wei);
-        }
-
-        return (_valueBlackNft + _valueGoldNft);
-    }
-
-    /// @dev function for the whitelisted users to buy NFT's between time frame 2 and time frame 3.
-    function userGetsNFTTF2(
-        address _to,
-        uint256 _id,
-        uint256 _valueBlackNft,
-        uint256 _valueGoldNft
-    ) public payable returns (uint256) {
-        require(
-            block.timestamp >= timeFrame2 && block.timestamp < timeFrame3,
-            "This time frame is over"
-        );
-        require(
-            whiteListedUsers[_to] == true,
-            "Address is not whitlisted and hence it can't buy NFT"
-        );
-        require(
-            (_valueBlackNft + _valueGoldNft) <= 3,
-            "User can't buy more than 3 NFTs in this timeframe."
-        );
-
-        if (_id == 1) {
-            safeTransferFrom(address(this), _to, _id, _valueBlackNft, "0x0");
-            addr.transfer(10000000000000000 wei);
-        }
-        if (_id == 2) {
-            safeTransferFrom(address(this), _to, _id, _valueGoldNft, "0x0");
-            addr.transfer(20000000000000000 wei);
-        }
-
-        return (_valueBlackNft + _valueGoldNft);
-    }
-
-    /// @dev function for the whitelisted users to buy NFT's between time frame 3 and time frame 4.
-    function userGetsNFTTF3(
-        address _to,
-        uint256 _id,
-        uint256 _valueBlackNft,
-        uint256 _valueGoldNft
-    ) public {
-        require(
-            block.timestamp >= timeFrame3 && block.timestamp < timeFrame4,
-            "This time frame is over"
-        );
-        require(
-            whiteListedUsers[_to] == true,
-            "Address is not whitlisted and hence it can't buy NFT"
-        );
-        uint256 tokenBlack = 20 * _valueBlackNft;
-        uint256 tokenGold = 40 * _valueGoldNft;
-        if (_id == 1) {
-            safeTransferFrom(_to, address(this), 1, _valueBlackNft, "0x0");
-            transferFrom(address(this), _to, tokenBlack);
-        }
-        if (_id == 2) {
-            safeTransferFrom(_to, address(this), 2, _valueBlackNft, "0x0");
-            transferFrom(address(this), _to, tokenGold);
-        }
-    }
-
-    /// @dev function for the whitelisted users to buy NFT's between time frame 4 and time frame 5.
-    function userGetsNFTTF4(
-        address _to,
-        uint256 _id,
-        uint256 _valueBlackNft,
-        uint256 _valueGoldNft
-    ) public {
-        require(
-            block.timestamp >= timeFrame4 && block.timestamp < timeFrame5,
-            "This time frame is over"
-        );
-        require(
-            whiteListedUsers[_to] == true,
-            "Address is not whitlisted and hence it can't buy NFT"
-        );
-        uint256 tokenBlack = 25 * _valueBlackNft;
-        uint256 tokenGold = 50 * _valueGoldNft;
-        if (_id == 1) {
-            safeTransferFrom(_to, address(this), 1, _valueBlackNft, "0x0");
-            transferFrom(address(this), _to, tokenBlack);
-        }
-        if (_id == 2) {
-            safeTransferFrom(_to, address(this), 1, _valueBlackNft, "0x0");
-            transferFrom(address(this), _to, tokenGold);
-        }
-    }
-
-    /// @dev function for the whitelisted users to buy NFT's between time frame 5 and time frame 6.
-    function userGetsNFTTF5(
-        address _to,
-        uint256 _id,
-        uint256 _valueBlackNft,
-        uint256 _valueGoldNft
-    ) public {
-        require(
-            block.timestamp >= timeFrame5 && block.timestamp < timeFrame6,
-            "This time frame is over"
-        );
-        require(
-            whiteListedUsers[_to] == true,
-            "Address is not whitlisted and hence it can't buy NFT"
-        );
-        uint256 tokenBlack = 50 * _valueBlackNft;
-        uint256 tokenGold = 100 * _valueGoldNft;
-        if (_id == 1) {
-            safeTransferFrom(_to, address(this), 1, _valueBlackNft, "0x0");
-            transferFrom(address(this), _to, tokenBlack);
-        }
-        if (_id == 2) {
-            safeTransferFrom(_to, address(this), 1, _valueBlackNft, "0x0");
-            transferFrom(address(this), _to, tokenGold);
-        }
     }
 }
